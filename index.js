af94fbdd3abe5a41aff4c8d43a475a53625aa3ac
Kiral-jessie <udejicynthia@gmail.com>
2024-12-19 09:50:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.querySelector(".nav-links"); 
-const authButton = document.querySelector(".auth-button"); // Use the class selector
+const authButton = document.querySelector(".auth-button"); 
 
 
 hamburger.addEventListener("click", () => {
