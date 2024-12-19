3afc4b04724f2181bde7ad8f16f854d31ceea9ea
Kiral-jessie <udejicynthia@gmail.com>
2024-12-19 09:48:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const hamburger = document.getElementById("hamburger");
-const navLinks = document.querySelector(".nav-links"); // Use the class selector
+const navLinks = document.querySelector(".nav-links"); 
+const authButton = document.querySelector(".auth-button"); // Use the class selector
+
 
 hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("active"); // Toggle the 'active' class on the mobile menu
+  navLinks.classList.toggle("active"); 
+authButton.classList.toggle("active"); 
 });
